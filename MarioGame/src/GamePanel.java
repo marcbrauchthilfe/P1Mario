1e67eec7ad59fe49a1b399a583d5063c3c531a42
marcbrauchthilfe <marc.filterbox@gmail.com>
2025-11-15 14:14:55 +0100

    Implemented ESC to close game

--- MarioGame/src/GamePanel.java
+++ MarioGame/src/GamePanel.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 
 public class GamePanel extends JPanel implements java.awt.event.ActionListener {
@@ -129,6 +130,14 @@ public class GamePanel extends JPanel implements java.awt.event.ActionListener {
                     loadLevel(currentLevelIndex);
                     state = State.START;
                 }
+            }
+        });
+
+        // ESC: exit game
+        im.put(KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), "ESC_pressed");
+        am.put("ESC_pressed", new AbstractAction() {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
             }
         });
     }
